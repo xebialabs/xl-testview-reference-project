238a9580095690caadf6339a2419e801834b8feb
Udi Schneider <udisch@gmail.com>
2015-12-01 10:48:36 +0100

    add clean to script

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ mkdir test-results
 
 # karma
 cd karma
+rm TESTS-*.xml
 npm install
 ./node_modules/karma/bin/karma start karma.conf.js
 cd ..
@@ -13,28 +14,28 @@ cp karma/*.xml test-results/karma/
 
 # FitNesse
 cd fitnesse
-./gradlew testReference
+./gradlew cleanFitNesse testReference
 cd ..
 mkdir test-results/fitnesse
 cp -R fitnesse/FitNesseRoot test-results/fitnesse
 
 # gradle junit
 cd gradlejunit
-./gradlew test
+./gradlew clean test
 cd ..
 mkdir test-results/gradlejunit
 cp gradlejunit/build/test-results/*.xml test-results/gradlejunit
 
 # Maven surefire + junit
 cd mvnjunit
-mvn test
+mvn clean test
 cd ..
 mkdir test-results/mvnjunit
 cp mvnjunit/target/surefire-reports/*.xml test-results/mvnjunit
 
 # cucubumber + junit
 cd mvnjunitcucumber
-mvn test
+mvn clean test
 cd ..
 mkdir test-results/mvnjunitcucumber
 cp mvnjunitcucumber/target/surefire-reports/*.xml test-results/mvnjunitcucumber
@@ -42,14 +43,14 @@ cp mvnjunitcucumber/target/*.json test-results/mvnjunitcucumber
 
 # Maven Surefire + TestNG
 cd mvntestng
-mvn test
+mvn clean test
 cd ..
 mkdir test-results/mvntestng
 cp mvntestng/target/surefire-reports/*.xml test-results/mvntestng
 
 # Gradle + TestNG
 cd testng
-./gradlew test
+./gradlew clean test
 cd ..
 mkdir test-results/testng
 cp -R testng/build/test-results/*.xml test-results/testng
--- mvnjunitcucumber/src/test/java/com/xebialabs/xltest/reference/mvnjunitcucumber/p1/MvnJunitCucumber1Test.java
+++ mvnjunitcucumber/src/test/java/com/xebialabs/xltest/reference/mvnjunitcucumber/p1/MvnJunitCucumber1Test.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = false, features = "src/test/resources/com/xebialabs/xltest/reference/mvnjunitcucumber/p1", format = { "pretty",
-        "json:target/cucumber.json" }, tags = { "~@ignore" })
+        "json:target/cucumber-report.json" }, tags = { "~@ignore" })
 public class MvnJunitCucumber1Test {
 
 
