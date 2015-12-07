e55e6c713bf0345997d69b210edb548b9c8e55fc
Stefan Hendriks <stefan@fundynamic.com>
2015-12-07 11:35:42 +0100

    Use testresults instead of test-results because its more friendly
    - more friendly in packaging structure when testing xltv test result
      parsers. No dashes == nice.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf test-results
-mkdir test-results
+rm -rf testresults
+mkdir testresults
 
 # karma
 cd karma
@@ -12,49 +12,49 @@ rm -rf node_modules
 npm install
 ./node_modules/karma/bin/karma start karma.conf.js
 cd ..
-mkdir test-results/karma
-cp karma/*.xml test-results/karma/
+mkdir testresults/karma
+cp karma/*.xml testresults/karma/
 
 # FitNesse
 cd fitnesse
 ./gradlew cleanFitNesse testReference
 cd ..
-mkdir test-results/fitnesse
-cp -R fitnesse/FitNesseRoot test-results/fitnesse
+mkdir testresults/fitnesse
+cp -R fitnesse/FitNesseRoot testresults/fitnesse
 
 # gradle junit
 cd gradlejunit
 ./gradlew clean test
 cd ..
-mkdir test-results/gradlejunit
-cp gradlejunit/build/test-results/*.xml test-results/gradlejunit
+mkdir testresults/gradlejunit
+cp gradlejunit/build/test-results/*.xml testresults/gradlejunit
 
 # Maven surefire + junit
 cd mvnjunit
 mvn clean test
 cd ..
-mkdir test-results/mvnjunit
-cp mvnjunit/target/surefire-reports/*.xml test-results/mvnjunit
+mkdir testresults/mvnjunit
+cp mvnjunit/target/surefire-reports/*.xml testresults/mvnjunit
 
 # cucubumber + junit
 cd mvnjunitcucumber
 mvn clean test
 cd ..
-mkdir test-results/mvnjunitcucumber
-cp mvnjunitcucumber/target/surefire-reports/*.xml test-results/mvnjunitcucumber
-cp mvnjunitcucumber/target/*.json test-results/mvnjunitcucumber
+mkdir testresults/mvnjunitcucumber
+cp mvnjunitcucumber/target/surefire-reports/*.xml testresults/mvnjunitcucumber
+cp mvnjunitcucumber/target/*.json testresults/mvnjunitcucumber
 
 # Maven Surefire + TestNG
 cd mvntestng
 mvn clean test
 cd ..
-mkdir test-results/mvntestng
-cp mvntestng/target/surefire-reports/*.xml test-results/mvntestng
+mkdir testresults/mvntestng
+cp mvntestng/target/surefire-reports/*.xml testresults/mvntestng
 
 # Gradle + TestNG
 cd testng
 ./gradlew clean test
 cd ..
-mkdir test-results/testng
-cp -R testng/build/test-results/*.xml test-results/testng
+mkdir testresults/testng
+cp -R testng/build/test-results/*.xml testresults/testng
  
