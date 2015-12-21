7cabcef4aa04c884925480db0da8196e9c99cd88
Udi Schneider <udisch@gmail.com>
2015-12-21 14:16:12 +0100

    add inner class with Enclosed test reference

---
+++ gradlejunit/src/test/java/com/xebialabs/xltest/reference/JUnitEnclosed.java
@@ -0,0 +1,23 @@
+package com.xebialabs.xltest.reference;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ *  Tests executed with Enclosed and inner classes may generate duplicate test results.
+ */
+@RunWith(Enclosed.class)
+public class JUnitEnclosed {
+
+    public static class Inner {
+
+        @Test
+        public void insideInnerClass() {
+            assertTrue(true);
+        }
+    }
+
+}
