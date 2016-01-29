768fde7e4536cc0b3ec77fed492d1555ae3aca7f
Udi Schneider <udisch@gmail.com>
2016-01-29 13:03:16 +0100

    Add link to gradle bug with Enclosed runner

--- gradlejunit/src/test/java/com/xebialabs/xltest/reference/JUnitEnclosed.java
+++ gradlejunit/src/test/java/com/xebialabs/xltest/reference/JUnitEnclosed.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 import static junit.framework.TestCase.assertTrue;
 
 /**
- *  Tests executed with Enclosed and inner classes may generate duplicate test results in some reporters.
+ *  Tests executed with Enclosed and inner classes may generate duplicate test results in some build tools.
+ *  For a gradle bug with this behaviour see: https://github.com/gradle/gradle/pull/489
  */
 @RunWith(Enclosed.class)
 public class JUnitEnclosed {
